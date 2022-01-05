5d7db2bf8670bffe0673e8baa9dda2b301fba406
Dani <dani8439@aol.com>
2022-01-05 15:16:39 -0500

    Updated Palindrome with notes and multiple solutions.

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,26 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split("").every((char, i) => {
+    // to get the mirror index.
+    return char === str[str.length - i - 1];
+  });
+}
+
+// Anothor array helper. Array.prototype.every()
+// Does a boolean check on every element in an array. Ex is every value greater than 5?
+// array.every((val) => val > 5);
+// Does do more comparisons than necessary as it iterates through the entire array. Double comparison taking an array of a b c b a. it compares a to a, b to b, c to c, then does the second half and compares b to b, and a to a. etc. Double comparison.
+//   Not an ideal solution for this problem. Essentially twice as much work as we have to do. But it's so you can explain to your interviewer what he just said. It's too much work. Twice as much work as we need to do. If you can think of multiple solutions to a problem, highly recommend give them. In your interest to be aware of all the ways of doing it.
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("");
+
+//   //   if (str === reversed) return true;
+//   //   else return false;
+//   // can become
+//   return str === reversed;
+// }
