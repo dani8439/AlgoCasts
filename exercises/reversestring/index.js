462f54bd4264190dd6b16b9cfe0a6906965110f8
Dani <dani8439@aol.com>
2022-01-05 13:58:04 -0500

    Notes about use and debugging, solutions and explanations for reverse.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,45 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = "";
+
+  // instead of for (var i = 0; i < str.length; i++)
+  for (let character of str) {
+    reversed = character + reversed;
+    // debugger;
+  }
+  return reversed;
+}
+
+// reverse("asdf");
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// function reverse(str) {
+//   let reversed = "";
+
+//   // instead of for (var i = 0; i < str.length; i++)
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   //   debugger;
+//   // turn it into an array.
+//   // reduce is used to take all different values of an array, and condense into one single value. 2 args, arrow function, second is a starting initial value for our function
+
+//   // When reduce runs, it's going to take the starting argument, pass it into the arrow function as the first argument, whatever gets returned from that inner function, will be used as starting argument of each successive run of the function. In total function runs one time for every element in the array. First value passed in is our reversed string.
+//   return str.split("").reduce((reversed, character) => {
+//     return character + reversed;
+//   }, "");
+
+//   // can simplify syntax
+//   //   return str.split('').reduce((rev, char) => char + rev, '');
+// }
--- notes.md
+++ notes.md
@@ -13,3 +13,17 @@ If that gives you an error, try instead running `jest fib --watch`
 Whenever you change any code in the structure, the test will run manually. But if you want to run it again, can press the `enter` key on keyboard.
 
 Once done running all the tests on your terminal, press `CTRL + C` to stop the process from running.
+
+To pop into debugger when testing out your code, throw in a debugger statement. Then cd into whatever directory you're working in. You have to call the function with an argument.
+
+Then call `node inspect index.js` and it will run debugger.
+
+Can write `c` or `cont` to continue evaluating code.
+
+To inspect something have to do repl mode `repl` and can then type in `str` or variable name. or copy over code.
+
+`ctrl c` kicks you out of the repl and back to debug mode.
+
+and `ctrl c` will kick you out of it completely.
+
+If you place a debugger within a for loop, or `map` etc, it will run multiple times, and can keep clicking through.
