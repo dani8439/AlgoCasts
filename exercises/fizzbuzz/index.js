0efd82ec24137a43684594c345e3e37db46a63d6
Dani <dani8439@aol.com>
2022-01-12 14:02:17 -0500

    Updated FizzBuzz with solution.

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let num = 1; num <= n; num++) {
+    // is the number a multiple of 3 and 5?
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("fizzbuzz");
+      // is the number a multiple of 3?
+    } else if (num % 3 === 0) {
+      console.log("fizz");
+      // is the number a multiple of 5?
+    } else if (num % 5 === 0) {
+      console.log("buzz");
+      // console.log numbers that are not multiples of either
+    } else {
+      console.log(num);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
