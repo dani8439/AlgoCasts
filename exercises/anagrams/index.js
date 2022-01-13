c3a5f1b376057c089f0e82fe0c2ebb2c97f28193
Dani <dani8439@aol.com>
2022-01-13 14:44:56 -0500

    Anagrams solution 2.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,35 +9,43 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
-
-  // need to keep in mind edge case, and compare both character maps. Going to pull out all the keys and count them and compare for both.
-  // If the keys are wrong number, obviously not a match and immediately return false
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for (let char in aCharMap) {
-    // if char doesn't equal in both, something must be wrong
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-  // in case we escape both succesfully, it must be true
-  return true;
+  return cleanString(stringA) === cleanString(stringB);
 }
 
-function buildCharMap(str) {
-  const charMap = {};
-
-  // clean up logic in for loop.
-  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-    // charMap + 1, in the case there is none, set it to 1
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-
-  return charMap;
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 }
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   // need to keep in mind edge case, and compare both character maps. Going to pull out all the keys and count them and compare for both.
+//   // If the keys are wrong number, obviously not a match and immediately return false
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     // if char doesn't equal in both, something must be wrong
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   // in case we escape both succesfully, it must be true
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   // clean up logic in for loop.
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     // charMap + 1, in the case there is none, set it to 1
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
