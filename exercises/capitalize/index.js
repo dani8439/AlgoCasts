b64abceafdbfea01b5ac8eaff481b689cf4a4915
Dani <dani8439@aol.com>
2022-01-18 15:03:41 -0500

    Capitalize solution 2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,14 +8,27 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  const words = [];
+  let result = str[0].toUpperCase();
 
-  for (let word of str.split(" ")) {
-    // iterate through the words, (split array on spaces), capitalize the first character, and then add it to the slice of the rest of the word. Then push that onto our empty words array.
-    words.push(word[0].toUpperCase() + word.slice(1));
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
   }
-  // after iterating through all of our words and capitalizing, and rejoin together with the space into a sentence.
-  return words.join(" ");
+  return result;
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   const words = [];
+
+//   for (let word of str.split(" ")) {
+//     // iterate through the words, (split array on spaces), capitalize the first character, and then add it to the slice of the rest of the word. Then push that onto our empty words array.
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   // after iterating through all of our words and capitalizing, and rejoin together with the space into a sentence.
+//   return words.join(" ");
+// }
