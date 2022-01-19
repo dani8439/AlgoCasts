27b77138bd102e78b3c278577f973fad8a4d0dec
Dani <dani8439@aol.com>
2022-01-19 15:17:44 -0500

    Recursive solution for steps.

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,42 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += "#";
-      } else {
-        stair += " ";
-      }
-    }
-    console.log(stair);
+// first time we call steps, have n of 4, 5, 6, row is 0, stair is empty string.
+function steps(n, row = 0, stair = "") {
+  // want to pass along current row, so adding row as argument. Default as 0, top row of our problem
+  // pass in default of stair as well, with an empty string
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  // call steps again until we've gone through all the rows and we satisfy other logic.
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+// Solution 1
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
